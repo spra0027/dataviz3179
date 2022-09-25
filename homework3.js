d63d1e56a797a68a6c94b99372ba0cabba3f60ce
Shakthi <spra0027@student.monash.edu>
2022-09-25 15:33:39 +1000

    added a js file for script

---
+++ homework3.js
@@ -0,0 +1,4 @@
+var spec2 = "homework3.json";
+vegaEmbed('#choropleth_map', spec2).then(function(result) {
+  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
