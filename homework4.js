a1a32ee1f46e3ceb154d0f56eda3457e25a58509
Shakthi <spra0027@student.monash.edu>
2022-10-08 14:55:56 +1100

    home work done

--- homework4.js
+++ homework4.js
@@ -1,5 +1,5 @@
 var spec2 = "homework4.json";
-var spec3 = "homework4_2.json";
+var spec3 = "homework4.json";
 vegaEmbed('#choropleth_map', spec2).then(function(result) {
   // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
